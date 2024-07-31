652e9bcaa0ddf5bcaa0ba5e324d4938cd502d785
elena-pash <elenapashkova@mosspark.com>
2024-07-31 16:17:10 -0400

    change

--- middleware/multerConfig.js
+++ middleware/multerConfig.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 const path = require("path");
 const getFormattedDateTime = require("../util/dateTimeUtil");
 
-const storage = multer.diskStorage({
+const storage = multer.memoryStorage({
   destination: function (req, file, cb) {
     cb(null, path.join(__dirname, "../uploads"));
   },
--- router/upload_router.js
+++ router/upload_router.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
 const multerConfig = require("../middleware/multerConfig");
 const Image = require("../models/file");
-const fs = require("fs");
 
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../views/upload.html"));
@@ -11,25 +9,22 @@ router.get("/", (req, res) => {
 
 router.post("/", multerConfig.single("file"), (req, res) => {
   if (!req.file) {
+    console.error("No file uploaded.");
     return res.status(400).send("No file uploaded.");
   }
 
-  const filePath = path.join(__dirname, "../uploads", req.file.filename);
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      return res.status(500).send("Error reading file from disk.");
-    }
-
-    const newImage = new Image({
-      filename: req.file.originalname,
-      contentType: req.file.mimetype,
-      imageBuffer: data,
-    });
-
-    newImage.save()
-      .then(() => res.status(200).send("File uploaded successfully."))
-      .catch((error) => res.status(500).send("Error saving file to database."));
+  const newImage = new Image({
+    filename: req.file.originalname,
+    contentType: req.file.mimetype,
+    imageBuffer: req.file.buffer, // Use buffer directly
   });
+
+  newImage.save()
+    .then(() => res.status(200).send("File uploaded successfully."))
+    .catch((error) => {
+      console.error("Error saving file to database:", error);
+      res.status(500).send("Error saving file to database.");
+    });
 });
 
 router.get("/multiple", (req, res) => {
@@ -38,33 +33,31 @@ router.get("/multiple", (req, res) => {
 
 router.post("/multiple", multerConfig.array("files", 100), (req, res) => {
   if (!req.files || req.files.length === 0) {
+    console.error("No files uploaded.");
     return res.status(400).send("No files uploaded.");
   }
 
   const filePromises = req.files.map((file) => {
-    const filePath = path.join(__dirname, "../uploads", file.filename);
-    return new Promise((resolve, reject) => {
-      fs.readFile(filePath, (err, data) => {
-        if (err) {
-          return reject(err);
-        }
-
-        const newImage = new Image({
-          filename: file.originalname,
-          contentType: file.mimetype,
-          imageBuffer: data,
-        });
-
-        newImage.save()
-          .then(resolve)
-          .catch(reject);
-      });
+    const newImage = new Image({
+      filename: file.originalname,
+      contentType: file.mimetype,
+      imageBuffer: file.buffer, // Use buffer directly
     });
+
+    return newImage.save()
+      .then(() => console.log(`File ${file.originalname} uploaded and saved successfully.`))
+      .catch((error) => {
+        console.error(`Error saving file ${file.originalname} to database:`, error);
+        throw error;
+      });
   });
 
   Promise.all(filePromises)
     .then(() => res.status(200).send("Files uploaded successfully."))
-    .catch((error) => res.status(500).send("Error saving files to database."));
+    .catch((error) => {
+      console.error("Error saving files to database:", error);
+      res.status(500).send("Error saving files to database.");
+    });
 });
 
 module.exports = router;
