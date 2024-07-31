df1270471b7b809b9cc6802e7bb03f644dd3284c
elena-pash <elenapashkova@mosspark.com>
2024-07-31 13:24:01 -0400

    change to routes

--- index.js
+++ index.js
@@ -1,60 +1,39 @@
-require("dotenv").config();
-const PORT = process.env.PORT || 3000;
-const express = require("express");
-const app = express();
-const path = require("path");
-const upload_router = require("./router/upload_router");
-const fetch_router = require("./router/fetch_router");
-const mongoose = require("mongoose");
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const upload_router = require('./router/upload_router');
+const fetch_router = require('./router/fetch_router');
 
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 });
-
 let db = mongoose.connection;
-db.once("open", () => {
-  console.log("Connected to MongoDB");
-});
-db.on("error", (err) => {
-  console.error("DB Error:" + err);
-});
+db.once('open', () => console.log('Connected to MongoDB'));
+db.on('error', (err) => console.error('DB Error:' + err));
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
-app.use("/upload", upload_router);
-app.use("/fetch", fetch_router);
+app.use(express.static('public'));
 
-// Serve index.html
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/index.html"));
-});
+// Routes
+app.use('/upload', upload_router);
+app.use('/fetch', fetch_router);
 
-// Serve other views
-app.get("/gallery", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "gallery.html"));
-});
-
-app.get("/gallery-pagination", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "gallery-pagination.html"));
-});
-
-app.get("/fetch-random", (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/fetch-random.html"));
-});
-
-app.get("/fetch-multiple-random", (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/fetch-multiple-random.html"));
-});
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views/index.html')));
+app.get('/gallery', (req, res) => res.sendFile(path.join(__dirname, 'views/gallery.html')));
+app.get('/gallery-pagination', (req, res) => res.sendFile(path.join(__dirname, 'views/gallery-pagination.html')));
+app.get('/fetch-random', (req, res) => res.sendFile(path.join(__dirname, 'views/fetch-random.html')));
+app.get('/fetch-multiple-random', (req, res) => res.sendFile(path.join(__dirname, 'views/fetch-multiple-random.html')));
 
 // Handle 404
-app.use((req, res) => {
-  res.status(404).send("Route does not exist on our server");
-});
+app.use((req, res) => res.status(404).send('Route does not exist on our server'));
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
-});
+// Start server
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
--- router/upload_router.js
+++ router/upload_router.js
@@ -1,66 +1,52 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const path = require("path");
-const multer = require("multer");
+const multer = require('multer');
+const Image = require('../models/file');
+
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
-const Image = require("../models/file");
 
-// Upload single file
-router
-  .route("/")
-  .get((req, res) => {
-    res.sendFile(path.join(__dirname, "../views/upload.html"));
-  })
-  .post(upload.single("file"), (req, res) => {
-    if (!req.file) {
-      return res.status(400).send("No file uploaded.");
-    }
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/upload.html'));
+});
 
+router.post('/', upload.single('file'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
+
+  const newImage = new Image({
+    filename: req.file.originalname,
+    contentType: req.file.mimetype,
+    imageBuffer: req.file.buffer,
+  });
+
+  newImage.save()
+    .then(() => res.status(200).send('File uploaded successfully.'))
+    .catch((error) => res.status(500).send('Error saving file to database.'));
+});
+
+router.get('/multiple', (req, res) => {
+  res.sendFile(path.join(__dirname, '../views/upload-multiple.html'));
+});
+
+router.post('/multiple', upload.array('files', 100), (req, res) => {
+  if (!req.files || req.files.length === 0) {
+    return res.status(400).send('No files uploaded.');
+  }
+
+  const imagePromises = req.files.map((file) => {
     const newImage = new Image({
-      filename: req.file.originalname,
-      contentType: req.file.mimetype,
-      imageBuffer: req.file.buffer, // ensure buffer is used here
+      filename: file.originalname,
+      contentType: file.mimetype,
+      imageBuffer: file.buffer,
     });
-
-    newImage.save()
-      .then(() => {
-        res.status(200).send("File uploaded successfully.");
-      })
-      .catch((error) => {
-        console.log(error);
-        res.status(500).send("Error saving file to database.");
-      });
+    return newImage.save();
   });
 
-// Upload multiple files
-router
-  .route("/multiple")
-  .get((req, res) => {
-    res.sendFile(path.join(__dirname, "../views/upload-multiple.html"));
-  })
-  .post(upload.array("files", 100), (req, res) => {
-    if (!req.files || req.files.length === 0) {
-      return res.status(400).send("No files uploaded.");
-    }
-
-    const imagePromises = req.files.map((file) => {
-      const newImage = new Image({
-        filename: file.originalname,
-        contentType: file.mimetype,
-        imageBuffer: file.buffer, // ensure buffer is used here
-      });
-      return newImage.save();
-    });
-
-    Promise.all(imagePromises)
-      .then(() => {
-        res.status(200).send("Files uploaded successfully.");
-      })
-      .catch((error) => {
-        console.log(error);
-        res.status(500).send("Error saving files to database.");
-      });
-  });
+  Promise.all(imagePromises)
+    .then(() => res.status(200).send('Files uploaded successfully.'))
+    .catch((error) => res.status(500).send('Error saving files to database.'));
+});
 
 module.exports = router;
